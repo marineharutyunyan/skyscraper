edf5598f5578e68444903198d034c8a1f9539960
marineharutyunyan <m.marine.harutyunyan@gmail.com>
2022-02-01 00:32:52 +0400

    Checking rules and inserting values in the board.

--- skyscraper.js
+++ skyscraper.js
@@ -1,4 +1,10 @@
 const size = 4;
+const rules =  [
+	[2,3,1,2],
+	[2,2,3,1],
+	[2,1,2,2],
+	[2,4,3,1]
+];
 
 function initializeBoard () {
 	let a =  [
@@ -6,64 +12,124 @@ function initializeBoard () {
 		[0,0,0,0],
 		[0,0,0,0],
 		[0,0,0,0]
-	];	
-	let b =  [
-		[1,2,3,2],
-		[4,6,7,8],
-		[1,10,11,12],
-		[13,14,15,16]
-	]; 
+	];
 	return a;
 };
 
 function printBoard(board)
 {
-	let i = -1;
-	while(++i < size)
-	{
-		let j = -1;
-		while(++j < size)
-		{
-			//console.log("i = ",board[i], "j = ", board[j]);
-			console.log(board[i][j]);
-			//console.log(board);
-		}
-	}
+	console.log(board);
 }
 
-function checkDuplicates(board)
+function checkLeftRight(x, board, size, rules)
 {
 	let i = -1;
-	while(++i < size)
-	{
-		let j = -1;
-		while(++j < size)
+	let max = 0;
+	let counter = 0;
+	
+	while (++i < size)
+	{	
+		if(max < board[x][i])
 		{
-			let k = j;
-			while(++k < size)
-			{
-				// check horizontal
-				let itemToBeCompared = board[i][j];
-				if( board[i][k] === itemToBeCompared)
-				{
-					console.log("duplicated item in the row", board[i][k]); 
-					return 1;
-				}
-
-				//check vertical 
-				itemToBeCompared = board[j][i];
-				if (board[k][i] === itemToBeCompared)
-				{
-					console.log("duplicated item in the column", board[k][i]); 
-					return 1;
-				}
-			}
+			max = board[x][i];
+			counter++; 
 		}
 	}
-	return 0
+
+	if(counter != rules[2][x])
+		return 0;
+
+	i = size;
+	max = 0;
+	counter = 0;
+	while (--i >= 0)
+	{		
+		if(max < board[x][i])
+		{
+			max = board[x][i];
+			counter++; 
+		} 		
+	}
+
+	if(counter != rules[3][x])
+		return 0;
+	return 1;
+}
+
+function checkUpDown(y, board, size, rules)
+{
+	let i = -1;
+	let max = 0;
+	let counter = 0;
+	while(++i < size)
+	{
+		if (max < board[i][y])
+		{
+			max = board[i][y];
+			counter++;
+		}
+	}
+	if(counter != rules[0][y])
+		return 0;
+	i = size;
+	max = 0;
+	counter = 0;
+	while (--i >= 0)
+	{		
+		if(max < board[i][y])
+		{
+			max = board[i][y];
+			counter++; 
+		} 		
+	}
+	if(counter != rules[1][y])
+		return 0;
+	return 1;
+}
+
+function check(board, x, y)
+{
+	let i = -1;
+	while (++i < size)
+	{
+		if (board[i][y] == board[x][y] && i != x && board[i][y] != 0)
+			return 0;
+		if (board[x][i] == board[x][y] && i != y && board[x][i] != 0)
+			return 0;
+	}
+	if (x == size - 1)
+		if (!checkUpDown(y, board, size, rules))
+			return 0;
+	if (y == size - 1)
+		if (!checkLeftRight(x, board, size, rules))
+			return 0;	
+	return 1
+}
+
+function insertValue(board, size, x, y)
+{
+    let value = 0;
+    let is_end;
+    while(++value <= size)
+    {
+        board[x][y] = value;
+        if(check(board, x, y))
+        {
+        	if(y+1 < size)
+        		is_end = insertValue(board, size, x, y+1)
+        	else if (x + 1 < size)
+				is_end = insertValue(board, size, x + 1, 0);
+			else
+            	return 1;
+            if (is_end)
+				return (1);
+        }
+    }
+
+    board[x][y] = 0;
+    return 0;
 }
 
 let board = initializeBoard();
+insertValue(board, size, 0, 0);
 printBoard(board);
-checkDuplicates(board);
-let number = 0;
